e65e4fb2f44d1dba6e680bd462cecfb93b6609a7
Nate Tessman <nate@pixalate.com>
2021-11-30 11:55:33 -0800

    specify nullability

--- prebid-blocking/Classes/PXDefaultBlockingStrategy.h
+++ prebid-blocking/Classes/PXDefaultBlockingStrategy.h
@@ -21,10 +21,10 @@
 - (instancetype _Nonnull) initWithTTL:(double)ttl;
 - (instancetype _Nonnull) initWithTTL:(double)ttl timeoutInterval:(double)timeoutInterval;
 
-- (void) getDeviceIdImpl:(PXBlockingStrategyResultHandler)resultHandler;
-- (void) getIPv4AddressImpl:(PXBlockingStrategyResultHandler)resultHandler;
-- (void) getIPv6AddressImpl:(PXBlockingStrategyResultHandler)resultHandler;
-- (void) getUserAgentImpl:(PXBlockingStrategyResultHandler)resultHandler;
+- (void) getDeviceIdImpl:(PXBlockingStrategyResultHandler _Nonnull)resultHandler;
+- (void) getIPv4AddressImpl:(PXBlockingStrategyResultHandler _Nonnull)resultHandler;
+- (void) getIPv6AddressImpl:(PXBlockingStrategyResultHandler _Nonnull)resultHandler;
+- (void) getUserAgentImpl:(PXBlockingStrategyResultHandler _Nonnull)resultHandler;
 
 @end
 
